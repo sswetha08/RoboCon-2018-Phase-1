c1f07112c027d70a82927d09df1ddcc5adc0e964
akshatha-k <33278067+akshatha-k@users.noreply.github.com>
2017-12-08 15:18:18 +0530

    Add files via upload

---
+++ MotionControl/TestCodes/check1.ino
@@ -0,0 +1,25 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial3.begin(9600);
+
+
+}
+// put your setup code here, to run once:
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (Serial3.available()) {
+    Serial.print("Value1 obtained from motion : ");
+  //  int value1 =  Serial3.parseInt();
+    Serial.println((char)Serial3.read());
+  }
+  if (Serial.available()) {
+    byte valsend = Serial.read();
+    Serial3.write(valsend);
+  }
+}
+
+
