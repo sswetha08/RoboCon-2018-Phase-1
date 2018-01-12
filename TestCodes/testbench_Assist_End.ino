8bfdbcc9e2a3de254dc16120190bdbeb8bea7621
tejaskashinath <33388221+tejaskashinath@users.noreply.github.com>
2018-01-12 20:57:12 +0530

    Add files via upload

---
+++ TestCodes/testbench_Assist_End.ino
@@ -0,0 +1,28 @@
+char hash;
+void setup() 
+{
+  Serial1.begin(9600);
+  Serial2.begin(9600);
+}
+
+void loop() 
+{
+  if(Serial2.available())
+  {
+    hash = Serial2.read();
+    if(hash == "#bm")
+    {
+      if(Serial1.available())
+      {
+        Serial1.write("#bm");
+      }
+    }
+    else if(hash == "#l")
+    {
+      if(Serial1.available())
+      {
+        Serial1.write("#l");
+      }
+    }
+  }
+}
