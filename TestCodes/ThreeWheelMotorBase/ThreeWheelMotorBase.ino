67828715c10da600b992e18851001b3c44d96d73
TheProjectsGuy <123avneesh@gmail.com>
2018-01-19 18:01:46 +0530

    Changed pinMode codes
    Added patchwork

--- TestCodes/ThreeWheelMotorBase/ThreeWheelMotorBase.ino
+++ TestCodes/ThreeWheelMotorBase/ThreeWheelMotorBase.ino
@@ -1,13 +1,13 @@
 class ThreeWheelBotMotors {
-  /*
-   * Important functions :
-   * * attachPWM_DIRPins(PWMs[], DIRs[])
-   * * moveAtWithAngle(int PWM, int ANGLE_DEGREES)
-   * * increasePWMs(value (s))
-   * * attachDebuggerSerial(&Serial)
-   * * enableDebugger()
-   * * disableDebugger()
-   */
+    /*
+       Important functions :
+     * * attachPWM_DIRPins(PWMs[], DIRs[])
+     * * moveAtWithAngle(int PWM, int ANGLE_DEGREES)
+     * * increasePWMs(value (s))
+     * * attachDebuggerSerial(&Serial)
+     * * enableDebugger()
+     * * disableDebugger()
+    */
     int MOTOR_MAG[3];
     int MAX_MAG;
     int MOTOR_MAG_PINS[3];
@@ -29,14 +29,17 @@ class ThreeWheelBotMotors {
     }
   public:
     // Attach Pins
-    void attachPWM_DIRPins(int PWM_PINS[],int DIR_PINS[]) {
-      attachPWMPins(PWM_PINS[0],PWM_PINS[1],PWM_PINS[2]);
-      attachDIRPins(DIR_PINS[0],DIR_PINS[1],DIR_PINS[2]);
+    void attachPWM_DIRPins(int PWM_PINS[], int DIR_PINS[]) {
+      attachPWMPins(PWM_PINS[0], PWM_PINS[1], PWM_PINS[2]);
+      attachDIRPins(DIR_PINS[0], DIR_PINS[1], DIR_PINS[2]);
     }
     void attachDIRPins(int dir_M1, int dir_M2, int dir_M3) {
       MOTOR_DIR_PINS[0] = dir_M1;
       MOTOR_DIR_PINS[1] = dir_M2;
       MOTOR_DIR_PINS[2] = dir_M3;
+      pinMode(dir_M1, OUTPUT);
+      pinMode(dir_M2, OUTPUT);
+      pinMode(dir_M3, OUTPUT);
       if (DEBUGGER_MODE) {
         debuggerSerial->print("\nMotor DIR Pins attached to ");
         debuggerSerial->print(dir_M1);
@@ -50,6 +53,9 @@ class ThreeWheelBotMotors {
       MOTOR_MAG_PINS[0] = PWM_M1;
       MOTOR_MAG_PINS[1] = PWM_M2;
       MOTOR_MAG_PINS[2] = PWM_M3;
+      pinMode(PWM_M1, OUTPUT);
+      pinMode(PWM_M2, OUTPUT);
+      pinMode(PWM_M3, OUTPUT);
       if (DEBUGGER_MODE) {
         debuggerSerial->print("\nMotor PWM Pins attached to ");
         debuggerSerial->print(PWM_M1);
@@ -72,7 +78,7 @@ class ThreeWheelBotMotors {
     //Debugger Mode
     void attachDebuggerSerial(HardwareSerial *s) {
       debuggerSerial = s;
-      DEBUGGER_MODE = true; 
+      DEBUGGER_MODE = true;
     }
     void disableDebugger() {
       DEBUGGER_MODE = false;
@@ -101,15 +107,15 @@ class ThreeWheelBotMotors {
     }
     void moveAtWithAngle_RAD(int PWM, int ANGLE) {
       /*
-       * Derived DCM Equations 
-       * Equations :->
-       [
+         Derived DCM Equations
+         Equations :->
+        [
         1, 1, 1;
         1, -cos(60), -cos(60);
         0, -sin(60), sin(60)
-       ] * [v1;v2;v3] = [0; V Sin(THETA) ; V Cos(THETA)]
+        ] * [v1;v2;v3] = [0; V Sin(THETA) ; V Cos(THETA)]
         ----- 0 degrees is the wheel 1 (index 0), angle taken counter clockwise
-       * WARNING : DO NOT CHANGE IF NOT UNDERSTOOD
+         WARNING : DO NOT CHANGE IF NOT UNDERSTOOD
          V1 = 0.6666666 * (PWM) * sin(ANGLE)
          V2 = -0.3333333 * (PWM) * sin(ANGLE) - 0.57735027 * (PWM) * cos(ANGLE)
          V3 = -0.3333333 * (PWM) * sin(ANGLE) + 0.57735027 * (PWM) * cos(ANGLE)
@@ -119,7 +125,7 @@ class ThreeWheelBotMotors {
       setMotorPWM(2, -0.33333333 * PWM * sin(ANGLE) + 0.57735027 * PWM * cos(ANGLE));
     }
     void moveAtWithAngle_DEG(int PWM, int ANGLE_DEG) {
-      moveAtWithAngle_DEG(PWM, float(ANGLE_DEG) * PI/180.0);
+      moveAtWithAngle_DEG(PWM, float(ANGLE_DEG) * PI / 180.0);
     }
 
 };
@@ -127,13 +133,13 @@ class ThreeWheelBotMotors {
 
 ThreeWheelBotMotors motors;
 
-int MotorPWMs[3] = {1,2,3};
-int MotorDIRs[3] = {10,11,12};
+int MotorPWMs[3] = {1, 2, 3};
+int MotorDIRs[3] = {10, 11, 12};
 void setup() {
   Serial.begin(9600);
   motors.attachDebuggerSerial(&Serial);
-  motors.attachPWM_DIRPins(MotorPWMs,MotorDIRs);
+  motors.attachPWM_DIRPins(MotorPWMs, MotorDIRs);
 }
-void loop() {  
+void loop() {
 }
 
