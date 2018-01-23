20d09b11bce1380bd334aedc11a105a66386e1bf
TheProjectsGuy <123avneesh@gmail.com>
2018-01-23 23:54:55 +0530

    Organised subfolder

---
+++ TestCodes/IMU/IMU_testing/IMU_testing.ino
@@ -0,0 +1,20 @@
+#include<SoftwareSerial.h>
+
+SoftwareSerial IMU(6,5);  //Rx, Tx
+
+void setup() {
+  Serial.begin(57600);
+  IMU.begin(57600);
+}
+
+void loop() {
+  while (IMU.available()) {
+    char c = IMU.read();
+    if (c == 'Y') {
+      Serial.print("Yaw is : ");
+      int angle = IMU.parseInt();
+      Serial.println(angle);
+    }
+  }
+}
+
